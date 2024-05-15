46037ca4de06528b8020ba47b2eaf665006ec686
AnnaVolkodav <annvolkodav@gmail.com>
2024-05-15 22:53:12 +0300

    added the basic information in js/html files

---
+++ js/gallery.js
@@ -0,0 +1,73 @@
+const images = [
+    {
+    preview:
+        'https://cdn.pixabay.com/photo/ 2019/05/14/16/43 /rchids- 420282 0__480.jpg',
+    original:
+        'https://cdn.pixabay.com/photo/ 2019/05/14/16/43 /rchids- 420282 0_ 1280 .jpg',
+    description: 'Hokkaido Flower',
+    },
+
+    {
+    preview:
+        'https://cdn.pixabay.com/photo/ 2019/05/14/22/05 /container- 420367 7__340.jpg',
+    original:
+        'https://cdn.pixabay.com/photo/ 2019/05/14/22/05 /container- 420367 7_ 1280 .jpg',
+    description: 'Container Haulage Freight',
+    },
+
+    {
+    preview:
+        'https://cdn.pixabay.com/photo/ 2019/05/16/09/47 /beach- 420678 5__340.jpg',
+    original:
+        'https://cdn.pixabay.com/photo/ 2019/05/16/09/47 /beach- 420678 5_ 1280 .jpg',
+    description: 'Aerial Beach View',
+    },
+
+    {
+    preview:
+        'https://cdn.pixabay.com/photo/ 2016/11/18/16/19 /flowers- 183561 9__340.jpg',
+    original:
+        'https://cdn.pixabay.com/photo/ 2016/11/18/16/19 /flowers- 183561 9_ 1280 .jpg',
+    description: 'Flower Blooms',
+    },
+
+    {
+    preview:
+        'https://cdn.pixabay.com/photo/ 2018/09/13/10/36 /mountains- 367433 4__340.jpg',
+    original:
+        'https://cdn.pixabay.com/photo/ 2018/09/13/10/36 /mountains- 367433 4_ 1280 .jpg',
+    description: 'Alpine Mountains',
+    },
+
+    {
+    preview:
+        'https://cdn.pixabay.com/photo/ 2019/05/16/23/04 /landscape- 420857 1__340.jpg',
+    original:
+        'https://cdn.pixabay.com/photo/ 2019/05/16/23/04 /landscape- 420857 1_ 1280 .jpg',
+    description: 'Mountain Lake Sailing',
+    },
+
+    {
+    preview:
+        'https://cdn.pixabay.com/photo/ 2019/05/17/09/27 /the-alps- 420927 2__340.jpg',
+    original:
+        'https://cdn.pixabay.com/photo/ 2019/05/17/09/27 /the-alps- 420927 2_ 1280 .jpg',
+    description: 'Alpine Spring Meadows',
+    },
+
+    {
+    preview:
+        'https://cdn.pixabay.com/photo/ 2019/05/16/21/10 /landscape- 420825 5__340.jpg',
+    original:
+        'https://cdn.pixabay.com/photo/ 2019/05/16/21/10 /landscape- 420825 5_ 1280 .jpg',
+    description: 'Nature Landscape',
+    },
+
+    {
+    preview:
+        'https://cdn.pixabay.com/photo/ 2019/05/17/04/35 /lighthouse- 420884 3__340.jpg',
+    original:
+        'https://cdn.pixabay.com/photo/ 2019/05/17/04/35 /lighthouse- 420884 3_ 1280 .jpg',
+    description: 'Lighthouse Coast Sea',
+    },  
+];
